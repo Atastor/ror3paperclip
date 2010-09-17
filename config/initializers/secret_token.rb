814441f79f164c7038d05260733a939b13ecc238
Michael Portz <michael.portz@accom.de>
2010-09-17 13:44:58 +0200

    Initiale Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ror3paperclip::Application.config.secret_token = '6f90dcae93ae3c1960887ed4a9f685328a09d19b6a362b1e1c6e943446234c47e3da2bcedcaa4699d6bcb7a5053f4beff6f3cc684dd9b66a7fc94f52e0ce8406'
